da946b929b9257d2cc5f6d680587bc0379616f24
Kaleidea <kaleidealogy@gmail.com>
2021-11-21 09:17:06 +0100

    Blink: Add <search> element - to document collections
    - document_statistics_collector.cc::CollectFeatures()
    - DocumentAllNameCollection::ElementMatches()
    - html_collection.cc::NameShouldBeVisibleInDocumentAll()

--- third_party/blink/renderer/core/html/html_collection.cc
+++ third_party/blink/renderer/core/html/html_collection.cc
@@ -30,6 +30,7 @@
 #include "third_party/blink/renderer/core/html/document_name_collection.h"
 #include "third_party/blink/renderer/core/html/forms/html_data_list_options_collection.h"
 #include "third_party/blink/renderer/core/html/forms/html_form_control_element.h"
+#include "third_party/blink/renderer/core/html/forms/html_form_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_option_element.h"
 #include "third_party/blink/renderer/core/html/forms/html_options_collection.h"
 #include "third_party/blink/renderer/core/html/html_element.h"
@@ -336,7 +337,7 @@ static inline bool NameShouldBeVisibleInDocumentAll(
   return element.HasTagName(html_names::kATag) ||
          element.HasTagName(html_names::kButtonTag) ||
          element.HasTagName(html_names::kEmbedTag) ||
-         element.HasTagName(html_names::kFormTag) ||
+         IsA<HTMLFormElement>(element) ||
          element.HasTagName(html_names::kFrameTag) ||
          element.HasTagName(html_names::kFramesetTag) ||
          element.HasTagName(html_names::kIFrameTag) ||
