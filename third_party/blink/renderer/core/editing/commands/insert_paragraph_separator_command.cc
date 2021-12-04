dcf0e0e1c7c7c6ec3143d5cd8ebdaae8e1efdb91
Kaleidea <kaleidealogy@gmail.com>
2021-12-04 22:03:54 +0100

    Blink: Add <search> element - includes for RTTI

--- third_party/blink/renderer/core/editing/commands/insert_paragraph_separator_command.cc
+++ third_party/blink/renderer/core/editing/commands/insert_paragraph_separator_command.cc
@@ -40,6 +40,7 @@
 #include "third_party/blink/renderer/core/html/html_br_element.h"
 #include "third_party/blink/renderer/core/html/html_element.h"
 #include "third_party/blink/renderer/core/html/html_quote_element.h"
+#include "third_party/blink/renderer/core/html/forms/html_form_element.h"
 #include "third_party/blink/renderer/core/html_names.h"
 #include "third_party/blink/renderer/core/layout/layout_object.h"
 #include "third_party/blink/renderer/core/layout/layout_text.h"
